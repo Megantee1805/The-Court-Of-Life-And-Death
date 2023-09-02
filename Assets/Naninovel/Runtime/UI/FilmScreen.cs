dc9c07cf168ab4fc997c6d0728f9af033f339265
Megantee1805 <megan.tee1805@gmail.com>
2023-09-03 00:44:35 +0800

    progress for today

---
+++ Assets/Naninovel/Runtime/UI/FilmScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilmScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* protected async void OnButtonClick()
+    {
+        titleMenu.Hide();
+    }
+    */
+}
